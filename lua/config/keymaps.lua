1ebd8d0d3cf23d779d8995c24b534c663f09d90a
Laurenz Guevara <laurenzguevara@outlook.com>
2024-10-02 17:05:55 +0100

    feat: Add new plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,45 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function()
+  harpoon:list():add()
+end)
+vim.keymap.set("n", "<C-e>", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+vim.keymap.set("n", "<leader>1", function()
+  harpoon:list():select(1)
+end)
+vim.keymap.set("n", "<leader>2", function()
+  harpoon:list():select(2)
+end)
+vim.keymap.set("n", "<leader>3", function()
+  harpoon:list():select(3)
+end)
+vim.keymap.set("n", "<leader>4", function()
+  harpoon:list():select(4)
+end)
+
+-- Shift V | J - Move line up or down
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+
+-- Keep cursor position after J
+map("n", "J", "mzJ`z")
+
+-- Center cursor after scrolling
+map("n", "<C-d>", "<C-d>zz", { remap = true })
+map("n", "<C-u>", "<C-u>zz", { remap = true })
